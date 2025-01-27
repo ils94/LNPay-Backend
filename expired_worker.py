8275aa9069a1ff968ffc13c8f6c9a988a4cccd9a
none <none@>
2025-01-27 12:56:32 -0300

    added MIT on files

--- expired_worker.py
+++ expired_worker.py
@@ -1,3 +1,25 @@
+# MIT License
+#
+# Copyright (c) 2025 ILS
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import asyncio
 import db
 import refund
@@ -27,7 +49,7 @@ async def process_invoice(invoice):
         is_valid = await asyncio.to_thread(db.is_invoice_valid_one_hour, invoice)
 
         if not is_valid:
-            print(f"deleting {invoice}, expiration was due on Strike API side...")
+            print(f"Deleting {invoice}, as its expiration was due on the Strike API side...")
             await asyncio.to_thread(db.delete_expired_invoice, invoice)
 
 
