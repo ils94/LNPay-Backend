d0aba241c36189cf9e7120a262358c30a32470e4
none <none@>
2025-01-28 12:55:13 -0300

    added a script to quickly generate a random key to be used when subscribing to Strike's webhook API.

---
+++ key_generator.py
@@ -0,0 +1,14 @@
+import random
+import string
+
+
+def generate_random_key(length):
+    characters = string.ascii_letters + string.digits  # Uppercase, lowercase, and digits
+    random_key = ''.join(random.choices(characters, k=length))
+    return random_key
+
+
+# You can use this script to generate a random 10 + character length key to create webhook subscriptions
+# Your key must be 10 in length, otherwise, Strike will return an error
+# No idea of the maximum size limit, but you can test it if you want :)
+generate_random_key(10)
